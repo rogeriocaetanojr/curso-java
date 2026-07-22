cb3de40edec9358dc635a6f11c385543a7517fd7
Rogério Caetano <rcaetanojunior@gmail.com>
2026-07-21 23:16:56 -0300

    feat: adiciona sexto programa em java

---
+++ fundamentos/Import.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+import javax.swing.*; // importando botao
+import java.util.Date; // importando datas
+
+public class Import {
+    public static void main(String[] args) {
+        Date d = new Date();
+        System.out.println(d);
+
+        JButton botao = new JButton();
+    }
+}
