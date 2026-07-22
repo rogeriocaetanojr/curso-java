fb28801f33d136fa2bc06cf53b3f27c16a786ee5
Rogério Caetano <rcaetanojunior@gmail.com>
2026-07-21 22:00:22 -0300

    feat: adiciona terceiro programa em java

---
+++ fundamentos/Temperatura.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class Temperatura {
+    public static void main(String[] args){
+       final double fator = 5 / 9.0;
+       final double ajuste = 32;
+
+       double fahrenheit = 86;
+       double celsius = (fahrenheit - ajuste) * fator;
+        System.out.println("O resultado é : " + celsius);
+
+        fahrenheit = 150;
+        celsius = (fahrenheit - ajuste) * fator;
+
+       System.out.println("O resultado é : " + celsius);
+
+    }
+}
