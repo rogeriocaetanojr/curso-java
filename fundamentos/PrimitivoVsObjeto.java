22e9f5faab1ff34c805a27af819af5ec4815ef21
Rogério Caetano <rcaetanojunior@gmail.com>
2026-07-22 00:38:39 -0300

    feat: adiciona nono programa em java

---
+++ fundamentos/PrimitivoVsObjeto.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class PrimitivoVsObjeto { // tudo em java é objeto, menos os tipos primitivos
+    public static void main(String[] args) {
+
+        String s = "texto";
+        String x = new String("texto");
+
+        //wrappers, é a versao objeto dos tipos primitivos
+        int a = 123;
+        System.out.println(a);
+
+        //TUDO EM JAVA É OBJETO, MENOS OS TIPOS PRIMITIVOS
+    }
+}
