a7270e35c5ee57da66ec2bbd7127dc7f4c4891c3
Rogério Caetano <rcaetanojunior@gmail.com>
2026-07-23 13:21:34 -0300

    feat: adiciona desafio de conversao usando scanner e tratando virgula para ponto com replace

---
+++ src/fundamentos/DesafioConversao.java
@@ -0,0 +1,27 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class DesafioConversao {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite seu primeiro salario: ");
+        String valor1 = entrada.next().replace(",", ".");
+
+        System.out.print("Digite seu segundo salario: ");
+        String valor2 = entrada.next().replace(",", ".");
+
+        System.out.print("Digite seu terceiro salario: ");
+        String valor3 = entrada.next().replace(",", ".");
+
+        double salario1 = Double.parseDouble(valor1);
+        double salario2 = Double.parseDouble(valor2);
+        double salario3 = Double.parseDouble(valor3);
+
+        double media = (salario1 + salario2 + salario3) / 3;
+        System.out.print("A media dos salarios é: " + media);
+
+        entrada.close();
+    }
+}
