146cc2423107c7893ad30baabc94bfe8812a0568
Rogério Caetano <rcaetanojunior@gmail.com>
2026-07-24 19:26:20 -0300

    feat(controle): adiciona programa de chat utilizando do while

---
+++ src/controle/DoWhile.java
@@ -0,0 +1,19 @@
+package controle;
+
+import java.util.Scanner;
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        String texto = "";
+
+        do{
+            System.out.println("Para sair do programa você precisa digitar as palavras mágicas...");
+            System.out.println("Quer sair?");
+            texto = entrada.nextLine();
+        } while(!texto.equalsIgnoreCase("por favor"));
+        System.out.println("Obrigado!");
+    }
+}
