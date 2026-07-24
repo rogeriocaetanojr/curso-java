5589074c311947f27fb8f615cce83d2cb1d261bc
Rogério Caetano <rcaetanojunior@gmail.com>
2026-07-24 18:23:47 -0300

    feat(controle): adiciona programa em java sobre while

---
+++ src/controle/WhileDeterminado.java
@@ -0,0 +1,14 @@
+package controle;
+
+public class WhileDeterminado {
+
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        while (contador <= 50){
+            System.out.printf("i = %d\n", contador);
+            contador++;
+        }
+    }
+}
