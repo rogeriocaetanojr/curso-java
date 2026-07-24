00cddc16b63a13deead335f3221b2637ffa18d5f
Rogério Caetano <rcaetanojunior@gmail.com>
2026-07-24 18:38:25 -0300

    feat(controle): adiciona programa em java sobre for

---
+++ src/controle/For1.java
@@ -0,0 +1,12 @@
+package controle;
+
+public class For1 {
+
+    public static void main(String[] args) {
+
+        for(int a = 1; a <= 5; a++){
+            System.out.printf("a = %d\n", a);
+        }
+    }
+}
+
