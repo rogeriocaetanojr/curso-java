4358708e7a3d82e4795b1eb66a717a544d6d060c
Rogério Caetano <rcaetanojunior@gmail.com>
2026-07-31 22:31:28 -0300

    refactor(classe): refatora os programas utilizando método

--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -14,11 +14,11 @@ public class ProdutoTeste {
         p2.preco = 2749.99;
         p2.desconto = 0.10;
 
-        double valorDescontoP1 = p1.preco * p1.desconto;
-        double valorFinalP1 = p1.preco - valorDescontoP1;
+       // double valorDescontoP1 = p1.preco * p1.desconto;
+        double valorFinalP1 = p1.precoComDesconto();
 
-        double valorDescontoP2 = p2.preco * p2.desconto;
-        double valorFinalP2 = p2.preco - valorDescontoP2;
+        //double valorDescontoP2 = p2.preco * p2.desconto;
+        double valorFinalP2 = p2.precoComDesconto();
 
         System.out.printf("O preço do Notebook com o desconto aplicado é de: R$%.2f \n", valorFinalP1);
         System.out.printf("O preço da Placa de Vídeo com o desconto aplicado é de: R$%.2f ", valorFinalP2);
