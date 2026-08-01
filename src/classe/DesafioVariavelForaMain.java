08c7da13d2bd9c9462ea4951f59c36aab770ea57
Rogério Caetano <rcaetanojunior@gmail.com>
2026-08-01 01:58:36 -0300

    feat(classe): adiciona exercicio para conseguir exibir variavel que esta fora do main

---
+++ src/classe/DesafioVariavelForaMain.java
@@ -0,0 +1,12 @@
+package classe;
+
+public class DesafioVariavelForaMain {
+
+    int a = 3; // nao pode mexer aqui
+
+    public static void main(String[] args) {
+
+        DesafioVariavelForaMain p = new DesafioVariavelForaMain();
+        System.out.println(p.a);
+    }
+}
